7f21f0b3a67c66875975089c568229a4314628a0
aluiziorenato <60110181+aluiziorenato@users.noreply.github.com>
2020-01-22 20:38:41 +0000

    Deploying to dist from  - 47988e07a00cac207b9c73f8cf2242ae554cd029 🚀

---
+++ precache-manifest.f1cb05bcfa8de545e9a50d3252cd89ba.js
@@ -0,0 +1,88 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/02095397cf5e39c66049.js"
+  },
+  {
+    "url": "/02095397cf5e39c66049.js.LICENSE"
+  },
+  {
+    "url": "/560c5da9efb13763c356.js"
+  },
+  {
+    "url": "/560c5da9efb13763c356.js.LICENSE"
+  },
+  {
+    "url": "/844b5c00f89edbbe5aaa.js"
+  },
+  {
+    "url": "/8be48107553d587c3eea.js"
+  },
+  {
+    "url": "/928bca3691330db893b3.js"
+  },
+  {
+    "revision": "71c3e6ebb6690cd17809",
+    "url": "/admin.js"
+  },
+  {
+    "revision": "80f7af4d0a827381c25623ceda5ca90a",
+    "url": "/admin.js.LICENSE"
+  },
+  {
+    "url": "/b641538c28718222d870.js"
+  },
+  {
+    "url": "/bc04f11e11a748165719.js"
+  },
+  {
+    "revision": "cb1da48994f1e2dee426",
+    "url": "/checkout.js"
+  },
+  {
+    "revision": "2189e9b93e0716399fab77dfabf220d2",
+    "url": "/checkout.js.LICENSE"
+  },
+  {
+    "url": "/d84ba18178e8c7e84447.js"
+  },
+  {
+    "url": "/edc5a325cf8ec123a6a1.js"
+  },
+  {
+    "url": "/fa95f606dea90c5dc911.js"
+  },
+  {
+    "url": "/fa95f606dea90c5dc911.js.LICENSE"
+  },
+  {
+    "url": "/icon_128x128.d78b0702273b38e5ef9dad1b22ebcd3e.png"
+  },
+  {
+    "url": "/icon_192x192.04a82673c3b1db2529af0d3ea40b403e.png"
+  },
+  {
+    "url": "/icon_384x384.1720bf874768ed5cafdb260a5e983183.jpeg"
+  },
+  {
+    "url": "/icon_512x512.7f97c8855218a4a7c1290eda1840210c.jpeg"
+  },
+  {
+    "url": "/icon_96x96.d1e9ad3fcb9d945d56172f9ee49dda0a.png"
+  },
+  {
+    "revision": "4ff15abaf3a3d7d334d9b7c7709a1093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "25324089407048a00403",
+    "url": "/storefront.css"
+  },
+  {
+    "revision": "25324089407048a00403",
+    "url": "/storefront.js"
+  },
+  {
+    "revision": "4afe187c776b0d05d44293da6186e677",
+    "url": "/storefront.js.LICENSE"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.7db2d9b125ae04a623f68a953656c6c2.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.f1cb05bcfa8de545e9a50d3252cd89ba.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* global workbox */
 
