485fbfa74eec55cbedcbaedbba51027e8e675490
Matheus <matheus@e-com.club>
2020-02-06 17:42:36 -0300

    feat: lenth guide 3

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,5 @@
 // Add your custom JavaScript for storefront pages here.
+const guide = new window.Modal(document.getElementById('modal-center'))
+window.showModalGuide = function () {
+  guide.show()
+}
