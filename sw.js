3d3407f3306885d705162b7956e0653010425c63
matheusgnreis <35343551+matheusgnreis@users.noreply.github.com>
2020-02-10 15:10:21 +0000

    Deploying to dist from  - 37c78cd10ffe5e0146aae7a1b22685b439aa3704 🚀

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.e0ef17fe3d802112b337ca6ca7cdaeb6.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.c93d9ccdfd1c5178ad34ab72a2427ffa.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* global workbox */
 
